61ee3bf452516a959f5daa748392b96a20286902
kurokaw3a <kamaldinov321@gmail.com>
2025-04-11 22:06:09 +0600

    maset

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,11 +1,8 @@
 import style from "./Layout.module.css";
 import { Outlet } from "react-router-dom";
-import { ClearStateByLocation } from "../hooks/router";
 import { Header } from "../components/Header/Header";
 
 export const Layout = () => {
-  ClearStateByLocation();
-
   return (
     <div className={style.layout}>
       <Header />
